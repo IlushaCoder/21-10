630e75e196a8461d21bc174de34ef5e071e4799f
BadAss <103047946+IlushaCoder@users.noreply.github.com>
2022-10-21 16:28:12 +0300

    Add files via upload

---
+++ is_it_prime/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static boolean isPrime(int num) {
+        if(num==1||num==0||num==-1) return false;
+        for(int i=2;i<=Math.sqrt(Math.abs(num));i++)
+        {
+            if(num%i==0 | num<0 )
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        System.out.println(isPrime(-5));
+    }
+
+}
